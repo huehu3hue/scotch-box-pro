d1cdcc857cc7a529eb96a94daea5b5c9232ac07c
Donald Barrett <donald.barrett@learningworks.co.nz>
2018-03-01 09:23:58 +1300

    Added pluginupdater, bash_profile and updated gitignore
    - Added .bash_profile with pluginupdater script
    - Updated .gitignore to ignore all additions to public dir and .provisioned files
    - Updated vagrant file

--- Vagrantfile
+++ Vagrantfile
@@ -21,6 +21,14 @@ if [ ! -f "/var/www/.provisioned_mycnf" ]; then
     touch /var/www/.provisioned_mycnf
 fi
 
+if [ ! -f "/var/www/.provisioned_pluginupdater" ]; then
+    echo Adding plugin updater.
+
+    cp /var/www/pluginupdater.sh /home/vagrant/
+    cp /var/www/.bash_profile /home/vagrant/
+    touch /var/www/.provisioned_pluginupdater
+fi
+
 if [ ! -d "/var/www/public/www" ]; then
     echo Making new www directory in /var/www/public/www
     mkdir /var/www/public/www
@@ -48,9 +56,15 @@ Vagrant.configure("2") do |config|
     config.vm.network "forwarded_port", guest: 80, host: 8080
     config.vm.network "private_network", ip: "192.168.33.10"
     config.vm.synced_folder ".", "/var/www", :mount_options => ["dmode=777", "fmode=666"]
+
+    # Copy host gitconfig and ssh.
+    config.vm.provision "file", source: "~/.gitconfig", destination: "~/.gitconfig"
+    config.vm.provision "file", source: "~/.ssh", destination: "~/.ssh"
+
     config.vm.provision "shell", inline: $installscript, privileged: true
 
     # Optional NFS. Make sure to remove other synced_folder line too
     #config.vm.synced_folder ".", "/var/www", :nfs => { :mount_options => ["dmode=777","fmode=666"] }
 
 end
+
---
+++ pluginupdater.sh
@@ -0,0 +1,31 @@
+function pluginupdater_addmodified() {
+	pluginupdater_setup
+	php /home/vagrant/scripts/moodle-pluginupdater/addmodified.php
+}
+
+function pluginupdater_adduntracked() {
+	pluginupdater_setup
+	php /home/vagrant/scripts/moodle-pluginupdater/adduntracked.php
+}
+
+function pluginupdater_setup() {
+	if [ ! -d /home/vagrant/scripts ]; then
+		mkdir /home/vagrant/scripts
+	fi
+
+	if [ "$(ls -al /home/vagrant/scripts/moodle-pluginupdater)" ] && [ ! -f /home/vagrant/scripts/moodle-pluginupdater/adduntracked.php ] && [ ! -f /home/vagrant/scripts/moodle-pluginupdater/addmodified.php ] && [ ! -d /home/vagrant/scripts/moodle-pluginupdater/.git ]; then
+		# Clear any phoneys. Shouldn't be any phoneys though.
+		rm -rf /home/vagrant/scripts/moodle-pluginupdater
+	fi
+
+	if [ ! -d /home/vagrant/scripts/moodle-pluginupdater ]; then
+		echo Installing pluginupdater from gitlab.
+		git clone git@gitlab.learningworks.co.nz:troy/plugin-updater.git -b changes /home/vagrant/scripts/moodle-pluginupdater
+	fi
+}
+
+# Add modified files to the moodle site repo.
+alias addmodified="pluginupdater_addmodified"
+
+# Add untracked files to the moodle site repo.
+alias adduntracked="pluginupdater_adduntracked"
